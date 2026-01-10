51fe6461e1d1ed1dec316967715a44dc163fa404
Noe Cantu <eoncantu@gmail.com>
2026-01-10 00:13:17 +0000

    Can you make the sections in the add Member page bit wider. Right now, t

--- src/app/members/member-form-sheet.tsx
+++ src/app/members/member-form-sheet.tsx
@@ -352,7 +352,7 @@ export function MemberFormSheet({ member, children }: MemberFormSheetProps) {
           </DialogTitle>
         </DialogHeader>
 
-        <div className="flex-grow overflow-y-auto pr-6 pl-2 -ml-2">
+        <div className="flex-grow overflow-y-auto -mx-6 px-6">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
             <Card>
