1d5b84cbf92b5297e81774b942b38054b65f0198
Noe Cantu <eoncantu@gmail.com>
2026-01-15 21:20:05 +0000

    Header: Custom Logo Added

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,21 @@
-import Link from 'next/link';
-import { FaithConnectLogo } from '@/components/icons';
-import { NavMenu } from '@/components/layout/sidebar';
+import Image from "next/image";
+import Link from "next/link";
+import { NavMenu } from "@/components/layout/sidebar";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 flex h-16 items-center border-b bg-background px-4 md:px-6">
       <Link href="/" className="flex items-center gap-2 font-semibold">
-        <FaithConnectLogo className="h-6 w-6" />
-        <span className="font-semibold">FAITH Connect</span>
+        <Image
+          src="/FaithConnectLogoWhite.png"
+          alt="Faith Connect Logo"
+          width={128}
+          height={128}
+          className="h-24 w-32"
+        />
+        {/* <span className="font-semibold">FAITH Connect</span> */}
       </Link>
+
       <div className="ml-auto">
         <NavMenu />
       </div>
