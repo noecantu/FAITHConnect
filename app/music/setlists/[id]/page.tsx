dd67e04447564b6a2cd43b5d82cc10cc1ce48a2a
Noe Cantu <eoncantu@gmail.com>
2026-02-04 00:11:50 +0000

    Setlist: Cleanup

--- app/music/setlists/[id]/page.tsx
+++ app/music/setlists/[id]/page.tsx
@@ -70,8 +70,6 @@ export default function SetListDetailPage() {
   const formattedDate = format(new Date(setList.date), 'M/d/yy, h:mm a');
 
   return (
-    // <div className="w-full max-w-5xl mx-auto px-6 py-6 space-y-6">
-    // <div className="p-6 space-y-4">
     <div className="space-y-6">
       <PageHeader title={setList.title} subtitle={formattedDate}>
       </PageHeader>
