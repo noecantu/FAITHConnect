c892e0688203274260ceb82eb1991956d728a365
noecantu <noecantu@me.com>
2026-02-24 20:32:14 -0700

    Working On Build Errors

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import AttendancePageContent from "./AttendancePageContent";
+
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return <AttendancePageContent />;
