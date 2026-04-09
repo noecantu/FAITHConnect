851206a20bc8aa24fcf730d109e59478b1fd0ffd
noecantu <noecantu@me.com>
2026-04-09 14:03:15 -0600

    UI Footer Update

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,12 +11,16 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
+
+      <div className="flex-1 flex flex-col">
         <DashboardPage>
           {children}
         </DashboardPage>
+      </div>
+
       <AppFooter />
-    </>
+    </div>
   );
 }
