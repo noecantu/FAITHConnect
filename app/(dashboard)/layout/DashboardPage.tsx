5e1199aa6d95dfb68120d4edeaffb41621407403
noecantu <noecantu@me.com>
2026-04-17 11:28:28 -0600

    Dashboard Page: Padding Widened

--- app/(dashboard)/layout/DashboardPage.tsx
+++ app/(dashboard)/layout/DashboardPage.tsx
@@ -8,7 +8,7 @@ interface DashboardPageProps {
 export function DashboardPage({ children }: DashboardPageProps) {
   return (
     <div className="pt-10 pb-10 px-6 sm:px-8 md:px-12 lg:px-16">
-     <div className="max-w-7xl mx-auto space-y-6">
+     <div className="w-full space-y-6">
         {children}
       </div>
     </div>
